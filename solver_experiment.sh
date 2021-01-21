b825f49c26ee76fc72ea18a98d583534f802a52c
jiseongg <jiseongg301@gmail.com>
2021-01-21 16:05:09 +0900

    <init><kupl_exp>: experiment setting
    - add benchmarks build scripts
    - add a script and `.env` file for experiment

---
+++ solver_experiment.sh
@@ -0,0 +1,98 @@
+base="/home/vagrant/benchmarks/"
+result_base="/home/vagrant/experiment/klee-results"
+script_base=""
+bcfile=""
+output_dir=""
+
+set_output_dir() {
+	output_dir="${result_base}/$1-$2-$3-$4"
+}
+
+set_bcfile() {
+	pgm=$1
+	bcfile=$(find ${base} -name "${pgm}.bc" 2>/dev/null)
+}
+
+set_script_dir() {
+	script_base=$(cd "$(dirname $1)"; echo "${PWD}")
+}
+
+generate_sandbox() {
+
+	if [[ ! -d "${script_base}/sandbox" ]]; then
+		tar xvf "${script_base}/sandbox.tar" -C "${script_base}"
+	fi
+}
+
+run_klee() {
+	local pgm=$1
+	local solver
+	local max_time
+	local search_strategies
+	local search_opt
+
+	if [[ $# -eq 5 ]]; then
+		solver=$4
+		max_time=$5
+		search_strategies="$2-$3"
+		search_opt="--search=$2 --search=$3"
+	elif [[ $# -eq 4 ]]; then
+		solver=$3
+		max_time=$4
+		search_strategies=$2
+		search_opt="--search=$2"
+	else
+		echo "Invalid arguments"
+		exit
+	fi
+
+	set_output_dir ${pgm} ${search_strategies} ${solver} ${max_time}
+	set_bcfile ${pgm}
+
+	local optimzie="--optimize --simplify-sym-indices --disable-inlining"
+	local solver="--solver-backend=${solver} --use-forked-solver --use-cex-cache --use-query-log=solver:kquery"
+	local debug_inst="--debug-print-instructions=all:file"
+	local libs="--libc=uclibc --posix-runtime"
+	local output="--only-output-states-covering-new --output-module=true --output-source=true --output-stats=true --output-dir=${output_dir}"
+	local budgets="--max-sym-array-size=4096 --max-memory-inhibit=false --max-time=${max_time}"
+	local env="--env-file=${script_base}/test.env --run-in-dir=${script_base}/sandbox"
+	local opt="--switch-type=internal --watchdog"
+	local search="${search_opt} --use-batching-search --batch-instructions=10000"
+	local args="--sym-args 0 1 10 --sym-args 0 2 2 --sym-files 1 8 --sym-stdin 8 --sym-stdout"
+	
+	generate_sandbox
+	echo "KLEE running for ${pgm}"
+	ulimit -s unlimited && ulimit -n 1000000 && ulimit -c unlimited && \
+		klee \
+		${optimize} ${solver} ${libs} ${output} ${debug_inst} \
+		${budgets} ${env} ${opt} ${search} \
+		${bcfile} ${args} > "${script_base}/log.txt" 2>&1
+	echo "KLEE finished for ${pgm}"
+	rm -rf "${script_base}/sandbox"
+
+	mv "${script_base}/log.txt" "${output_dir}"
+}
+
+main() {
+	set_script_dir $0
+
+	if [[ ! -d ${result_base} ]]; then
+		mkdir -p "${result_base}"
+	fi
+	
+#	run_klee gcal nurs:md2u random-state stp 7200s
+#	run_klee gcal nurs:qc stp 7200s
+#	run_klee combine nurs:md2u random-state stp 7200s
+#	run_klee combine nurs:qc stp 7200s
+	run_klee trueprint nurs:md2u random-state stp 7200s
+#	run_klee trueprint nurs:qc stp 7200s
+
+#	run_klee gcal nurs:md2u random-state z3 7200s
+#	run_klee gcal nurs:qc z3 7200s
+#	run_klee combine nurs:md2u random-state z3 7200s
+#	run_klee combine nurs:qc z3 7200s
+#	run_klee trueprint nurs:md2u random-state z3 7200s
+#	run_klee trueprint nurs:qc z3 7200s
+}
+
+main
